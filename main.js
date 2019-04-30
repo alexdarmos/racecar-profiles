71704bc71ca1e41125e072ac9a6db6cb0d1c3f74
Alex D <Alex@zzperformance.local>
2019-04-30 14:55:29 -0400

    dragging cards to re-order working

--- main.js
+++ main.js
@@ -7,77 +7,74 @@ $(() => {
      let released = false;
      let targetPosition = null;
      let target = null;
-
-     console.log(cardCounter);
-
-
-
-     let isMoving = (target, targetPosition, offset) => {
+     let nextTarget = null;
+     let prevTarget = null;
+     let offset = null;
 
 
+
+
+
+     function isMoving() {
           let newPosition = targetPosition - offset;
-
           $(target).css("left", newPosition);
-          // console.log(`newPosition: ${newPosition}`);
-
-          // let isReleased = (released) => {
-               // if (released === true && targetPosition <= 90) {
-               //      $(target).css("z-index", 100);
-               //      cardCounter++;
-               //      console.log(cardCounter);
-               //      return false;
-               // }
-          // }
-
      }
 
-     // let isReleased = (targetPosition) => {
-     //      if (targetPosition <= 90) {
-     //           $(target).css("z-index", 100);
-     //           cardCounter++;
-     //           console.log(cardCounter);
-     //           return false;
-     //      }
-     // }
+     function isReleased() {
+          released = true;
+          console.log(targetPosition)
+
+          if (released === true && targetPosition <= 90) {
+               cardCounter++;
+
+               console.log(cardCounter);
+
+               // $(nextTarget).addAttr(`disabled`);
 
 
 
-     $(`.card-1`)
+               if (cardCounter === 1) {
+                    $(target).css({"left":`${30}px`, "top":`${480}px`, "z-index": 100});
+                    $(nextTarget).next().css({"left": `${0}px`, "top": `${-500}px`, "z-index": 200});
+                    $(target).next().css({"left": `${-30}px`, "top": `${23}px`, "z-index": 300});
 
-     .on("touchstart", function(e) {
-           targetPosition = e.targetTouches[0].pageX;
-
-     })
-     
-     
-     .on("touchstart touchmove", function (e) {
-          // console.log(e);
-          // console.log(`offset: ${e.target.offsetWidth}`);
-          let offset = e.target.offsetWidth;
-          let target = e.currentTarget;
-           targetPosition = e.targetTouches[0].pageX;
-          // console.log(targetPosition);
-          // console.log(targetPosition);
-          isMoving(target, targetPosition, offset);
-          released = false;
-
-
-     })
-          .on("touchend", function (e) {
-               //end touch  
-               // return released = true;
-               // isReleased(released);
-               released = true;
-               console.log(targetPosition)
-
-               if (released === true && targetPosition <= 90) {
-                    $(target).css("z-index", 100);
-                    cardCounter++;
-                    console.log(cardCounter);
-                    return false;
+               
+               } else if (cardCounter === 2) {
+                    $(target).css({"left":`${30}px`, "top":`${-23}px`, "z-index": 100});
+                    $(prevTarget).css({"left": `${0}px`, "top": `${500}px`, "z-index": 200});
+                    $(target).next().css({"left": `${-30}px`, "top": `${-479}px`, "z-index": 300});
+               } else {
+                    $(target).css({"left":`${30}px`, "top":`${-525}px`, "z-index": 100});
+                    $(target).prev().css({"left": `${0}px`, "top": `${0}px`, "z-index": 200});
+                    $(prevTarget).prev().css({"left": `${-30}px`, "top": `${525}px`, "z-index": 300});
+                    cardCounter = 0;
+                    return;
                }
 
-          })
+
+          }
+     }
+
+
+     $(`.card`).on("touchstart", function (e) {
+          targetPosition = e.targetTouches[0].pageX;
+
+     }).on("touchmove", function (e) {
+          offset = e.target.offsetWidth;
+          target = e.currentTarget;
+          targetPosition = e.targetTouches[0].pageX;
+          nextTarget = $(target).next();
+          prevTarget = $(target).prev();
+
+          isMoving();
+     }).on("touchend", function (e) {
+          isReleased();
+
+
+     })
+
+
+
 
 })
 
